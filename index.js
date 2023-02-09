a3a63f641775428882fbb419981850b8f3eea025
shubhamrasam <shubhamrasam5820@gmail.com>
2023-02-10 04:31:30 +0530

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+// let navName = document.querySelectorAll(".nav-name");
+// let navIcon = document.querySelectorAll('.nav-link');
+
+
+
+// for(icon of navIcon){
+
+//     icon.addEventListener('mouseover',function(e){
+
+//        e.target.nextElementSibling.classList.add('show');
+
+//     });
+     
+// }
+
+
+
+// for(icon of navIcon){
+
+//     icon.addEventListener('mouseout',function(e){
+
+//        e.target.nextElementSibling.classList.remove('show');
+       
+//     });
+     
+// }
+
+
